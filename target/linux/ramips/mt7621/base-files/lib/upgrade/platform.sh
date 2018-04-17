2278890b36f931002b3b847e90a87ce9dafb817a
Chen Minqiang <ptpt52@gmail.com>
2018-04-18 00:46:03 +0800

    ramips: sysupgrade compatable with breed

--- target/linux/ramips/mt7621/base-files/lib/upgrade/platform.sh
+++ target/linux/ramips/mt7621/base-files/lib/upgrade/platform.sh
@@ -96,9 +96,15 @@ platform_do_upgrade() {
 	xiaomi,mi-router-cr6606|\
 	xiaomi,mi-router-cr6608|\
 	xiaomi,mi-router-cr6609|\
-	xiaomi,redmi-router-ac2100|\
 	zyxel,nwa50ax|\
 	zyxel,nwa55axe)
+		nand_do_upgrade "$1"
+		;;
+	xiaomi,redmi-router-ac2100)
+		# this make it compatible with breed
+		dd if=/dev/mtd0 bs=64 count=1 2>/dev/null | grep -qi breed && CI_KERNPART_EXT="kernel_stock"
+		dd if=/dev/mtd7 bs=64 count=1 2>/dev/null | grep -o MIPS.*Linux | grep -qi X-WRT && CI_KERNPART_EXT="kernel_stock"
+		dd if=/dev/mtd7 bs=64 count=1 2>/dev/null | grep -o MIPS.*Linux | grep -qi NATCAP && CI_KERNPART_EXT="kernel0_rsvd"
 		nand_do_upgrade "$1"
 		;;
 	iodata,wn-ax1167gr2|\
