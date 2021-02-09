27ef339432764f9e6dff0066ca5645de8a22b364
Chen Minqiang <ptpt52@gmail.com>
2021-02-09 22:27:32 +0800

    ramips: some devices use mt753x driver
    These devices use mt753x:
     - xwrt_t-cpe1201k-v01
     - xwrt_t-cpe1200k-v01
     - xwrt_nxc200p
     - xwrt_puppies
     - netgear_r6220
     - netgear_wndr3700-v5
     - d-team_newifi-d2
     - gehua_ghl-r-001
     - xiaomi_redmi-router-ac2100
     - xiaomi_mi-router-ac2100
     - xiaomi_mi-router-3g
     - xiaomi_mi-router-4
     - phicomm_k2p
     - jcg_y2

--- target/linux/ramips/image/mt7621.mk
+++ target/linux/ramips/image/mt7621.mk
@@ -802,6 +802,8 @@ TARGET_DEVICES += dual-q_h721
 
 define Device/d-team_newifi-d2
   $(Device/dsa-migration)
+  DEVICE_COMPAT_VERSION := 1.0
+  DEVICE_COMPAT_MESSAGE := Config is compat with swconfig
   $(Device/uimage-lzma-loader)
   IMAGE_SIZE := 32448k
   DEVICE_VENDOR := Newifi
@@ -1017,6 +1019,8 @@ TARGET_DEVICES += firefly_firewrt
 
 define Device/gehua_ghl-r-001
   $(Device/dsa-migration)
+  DEVICE_COMPAT_VERSION := 1.0
+  DEVICE_COMPAT_MESSAGE := Config is compat with swconfig
   IMAGE_SIZE := 32448k
   DEVICE_VENDOR := GeHua
   DEVICE_MODEL := GHL-R-001
@@ -1426,6 +1430,8 @@ TARGET_DEVICES += jcg_q20
 define Device/jcg_y2
   $(Device/dsa-migration)
   $(Device/uimage-lzma-loader)
+  DEVICE_COMPAT_VERSION := 1.0
+  DEVICE_COMPAT_MESSAGE := Config is compat with swconfig
   IMAGE_SIZE := 16064k
   IMAGES += factory.bin
   IMAGE/factory.bin := $$(sysupgrade_bin) | check-size | jcg-header 95.1
@@ -1732,6 +1738,8 @@ endef
 
 define Device/netgear_r6220
   $(Device/netgear_sercomm_nand)
+  DEVICE_COMPAT_VERSION := 1.0
+  DEVICE_COMPAT_MESSAGE := Config is compat with swconfig
   DEVICE_MODEL := R6220
   SERCOMM_HWNAME := R6220
   SERCOMM_HWID := AYA
@@ -1899,6 +1907,8 @@ define Device/netgear_wndr3700-v5
   $(Device/dsa-migration)
   $(Device/netgear_sercomm_nor)
   $(Device/uimage-lzma-loader)
+  DEVICE_COMPAT_VERSION := 1.0
+  DEVICE_COMPAT_MESSAGE := Config is compat with swconfig
   IMAGE_SIZE := 15232k
   DEVICE_MODEL := WNDR3700
   DEVICE_VARIANT := v5
@@ -1947,6 +1957,8 @@ TARGET_DEVICES += oraybox_x3a
 
 define Device/phicomm_k2p
   $(Device/dsa-migration)
+  DEVICE_COMPAT_VERSION := 1.0
+  DEVICE_COMPAT_MESSAGE := Config is compat with swconfig
   IMAGE_SIZE := 15744k
   DEVICE_VENDOR := Phicomm
   DEVICE_MODEL := K2P
@@ -2557,6 +2569,8 @@ TARGET_DEVICES += winstars_ws-wn583a6
 
 define Device/xiaomi_nand_separate
   $(Device/dsa-migration)
+  DEVICE_COMPAT_VERSION := 1.0
+  DEVICE_COMPAT_MESSAGE := Config is compat with swconfig
   $(Device/uimage-lzma-loader)
   DEVICE_VENDOR := Xiaomi
   BLOCKSIZE := 128k
