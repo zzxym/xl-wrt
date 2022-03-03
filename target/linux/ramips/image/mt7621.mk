aa26f1d69fc5a22b999b82143c33792dc2252f6d
Chen Minqiang <ptpt52@gmail.com>
2022-03-03 18:57:49 +0800

    ramips: init xwrt_nxc2009e-v100

--- target/linux/ramips/image/mt7621.mk
+++ target/linux/ramips/image/mt7621.mk
@@ -2809,6 +2809,20 @@ define Device/xwrt_puppies
 endef
 TARGET_DEVICES += xwrt_puppies
 
+define Device/xwrt_nxc2009e-v100
+  $(Device/nand)
+  $(Device/uimage-lzma-loader)
+  IMAGE_SIZE := 120320k
+  DEVICE_COMPAT_VERSION := 1.0
+  DEVICE_COMPAT_MESSAGE := Config is compat with swconfig
+  DEVICE_VENDOR := XWRT
+  DEVICE_MODEL := NXC2009E-V100
+  IMAGES += factory.bin
+  IMAGE/factory.bin := append-kernel | pad-to $$(KERNEL_SIZE) | append-ubi | check-size
+  DEVICE_PACKAGES := uboot-envtools kmod-gsw150 kmod-i2c-gpio i2c-tools xs2184
+endef
+TARGET_DEVICES += xwrt_nxc2009e-v100
+
 define Device/xzwifi_creativebox-v1
   $(Device/dsa-migration)
   IMAGE_SIZE := 32448k
