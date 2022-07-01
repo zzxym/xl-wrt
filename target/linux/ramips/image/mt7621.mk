4c3aa6baa177e01427f12cb8b572dc18b045389a
Chen Minqiang <ptpt52@gmail.com>
2022-07-01 19:06:25 +0800

    ramips: init mt7621_xwrt_ac8000p

--- target/linux/ramips/image/mt7621.mk
+++ target/linux/ramips/image/mt7621.mk
@@ -2785,6 +2785,17 @@ define Device/xwrt_mac500f
 endef
 TARGET_DEVICES += xwrt_mac500f
 
+define Device/xwrt_ac8000p
+  $(Device/uimage-lzma-loader)
+  IMAGE_SIZE := 16064k
+  DEVICE_VENDOR := XWRT
+  DEVICE_MODEL := AC8000P
+  IMAGES += factory.bin
+  IMAGE/factory.bin := $$(IMAGE/sysupgrade.bin) | tenbay-factory AC8000P
+  DEVICE_PACKAGES := uboot-envtools kmod-i2c-gpio i2c-tools poe-controller
+endef
+TARGET_DEVICES += xwrt_ac8000p
+
 define Device/xwrt_nxc200p
   $(Device/uimage-lzma-loader)
   IMAGE_SIZE := 16064k
