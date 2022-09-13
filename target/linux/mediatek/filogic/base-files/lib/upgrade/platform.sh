0e287579b94beee9e1f378c089b4f32c27dcfaa2
Chen Minqiang <ptpt52@gmail.com>
2022-09-13 13:46:20 +0800

    mediatek: add alternative stock layout for Xiaomi Redmi Router AX6000
    In this implementation, the flash partition layout is adjusted to avoid
    modifying the uboot environment of mtdparts. This ensures that the 30M
    ubi_kernel partition remains aligned with the stock ubi partition, and
    the kernel volume is placed in it. This allows the stock uboot to boot
    from it without changing the mtdparts, which is useful for reverting back
    to the stock firmware using Xiaomi Firmware Tools. In actual testing,
    modifying mtdparts has been found to break Xiaomi Firmware Tools.
    
    1. use ARTIFACTS to generate initramfs-factory.ubi for easy installation.
    2. The NAND flash layout is changed to allow for reverting back to the stock firmware.
    3. Before performing sysupgrade, do some cleanup in platform_pre_upgrade to ensure a clean installation of OpenWRT.
    4. Setup the uboot env to ensure that the system always boot, which can be helpful for users who may forget to do this before sysupgrade in the initramfs.
    
    New flash instructions:
    1. Gain ssh access (see the link: https://openwrt.org/toh/xiaomi/redmi_ax6000#installation)
    2. Check current stock system
       COMMAND: `cat /proc/cmdline`
       sample OUTPUT: `console=ttyS0,115200n1 loglevel=8 firmware=1 uart_en=1`
       if firmware=1, current system is ubi1
       if firmware=0, current system is ubi0
    3. Setup nvram
       If the current system is ubi1, please set it up so that the next time it will boot from ubi:
    ```
    nvram set boot_wait=on
    nvram set uart_en=1
    nvram set flag_boot_rootfs=0
    nvram set flag_last_success=0
    nvram set flag_boot_success=1
    nvram set flag_try_sys1_failed=0
    nvram set flag_try_sys2_failed=0
    nvram commit
    ```
       If the current system is ubi, please set it up so that the next time it will boot from ubi1:
    ```
    nvram set boot_wait=on
    nvram set uart_en=1
    nvram set flag_boot_rootfs=1
    nvram set flag_last_success=1
    nvram set flag_boot_success=1
    nvram set flag_try_sys1_failed=0
    nvram set flag_try_sys2_failed=0
    nvram commit
    ```
    4. Flash initramfs-factory.ubi
       If the firmware=1, the current system is using ubi1. In this case, flash the image to ubi:
       `ubiformat /dev/mtd8 -y -f /tmp/initramfs-factory.ubi`
       If the firmware=0, the current system is using ubi. In this case, flash the image to ubi1:
       `ubiformat /dev/mtd9 -y -f /tmp/initramfs-factory.ubi`
    5. After rebooting, the system should now boot into the openwrt initramfs system. Log in via SSH and perform the necessary setup.
    ```
    fw_setenv boot_wait on
    fw_setenv uart_en 1
    fw_setenv flag_boot_rootfs 0
    fw_setenv flag_last_success 1
    fw_setenv flag_boot_success 1
    fw_setenv flag_try_sys1_failed 8
    fw_setenv flag_try_sys2_failed 8
    fw_setenv mtdparts "nmbm0:1024k(bl2),256k(Nvram),256k(Bdata),2048k(factory),2048k(fip),256k(crash),256k(crash_log),30720k(ubi),30720k(ubi1),51200k(overlay)"
    ```
       Here, set flag_last_success=1 and flag_try_sys1_failed >= 6, to ensure that the system always boots from system 0.
    6. Flash sysupgrade.bin via sysupgrade
    ```
    sysupgrade -n /tmp/squashfs-sysupgrade.bin
    ```
    
    For existing users of the Redmi AX6000 running OpenWrt, here are the steps to flash to this new layout:
    1. Revert the mtdparts back to its stock value, use the following command:
    ```
    fw_setenv mtdparts "nmbm0:1024k(bl2),256k(Nvram),256k(Bdata),2048k(factory),2048k(fip),256k(crash),256k(crash_log),30720k(ubi),30720k(ubi1),51200k(overlay)"
    ```
    2. Flash initramfs-factory.ubi
    ```
    mtd -r -e ubi write /tmp/initramfs-factory.ubi ubi
    ```
    3. After rebooting, the system will boot into the new openwrt-initramfs. Log in and perform a sysupgrade to complete the process.
    ```
    sysupgrade -n /tmp/squashfs-sysupgrade.bin
    ```
    
    Signed-off-by: Chen Minqiang <ptpt52@gmail.com>

--- target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
+++ target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
@@ -1,5 +1,53 @@
 REQUIRE_IMAGE_METADATA=1
 
+redmi_ax6000_nand_upgrade_cleanup()
+{
+	local mtdnum="$( find_mtd_index ubi )"
+	if [ ! "$mtdnum" ]; then
+		echo "cannot find ubi mtd partition ubi"
+		return 1
+	fi
+
+	local kern_mtdnum="$( find_mtd_index ubi_kernel )"
+	if [ ! "$kern_mtdnum" ]; then
+		echo "cannot find ubi_kernel mtd partition ubi_kernel"
+		return 1
+	fi
+
+	#XXX: for first time sysupgrade from initramfs, the ubi may contain old 'data' volume
+	local ubidev="$( nand_find_ubi ubi )"
+	if [ "$ubidev" ] && [ "$( nand_find_volume $ubidev data )" ]; then
+		ubidetach -m "$mtdnum"
+		ubiformat /dev/mtd$mtdnum -y
+		ubiattach -m "$mtdnum"
+	fi
+
+	#XXX: for first time sysupgrade from initramfs, the ubi_kernel may contain old 'rootfs' volume
+	local kern_ubidev="$( nand_find_ubi ubi_kernel )"
+	if [ "$kern_ubidev" ] && [ "$( nand_find_volume $kern_ubidev rootfs )" ]; then
+		ubidetach -m "$kern_mtdnum"
+		ubiformat /dev/mtd$kern_mtdnum -y
+		ubiattach -m "$kern_mtdnum"
+	fi
+
+	#setup uboot env to make sure it always boot, sometimes user may forget to setup this env before sysupgrade from initramfs
+	if ! fw_printenv -n flag_try_sys2_failed &>/dev/null; then
+		echo "uboot env cannot read/write, skip"
+		return 0
+	fi
+	flag_try_sys2_failed=$(fw_printenv -n flag_try_sys2_failed)
+	if test $flag_try_sys2_failed -lt 8; then
+		fw_setenv boot_wait on
+		fw_setenv uart_en 1
+		fw_setenv flag_boot_rootfs 0
+		fw_setenv flag_last_success 1
+		fw_setenv flag_boot_success 1
+		fw_setenv flag_try_sys1_failed 8
+		fw_setenv flag_try_sys2_failed 8
+		fw_setenv mtdparts "nmbm0:1024k(bl2),256k(Nvram),256k(Bdata),2048k(factory),2048k(fip),256k(crash),256k(crash_log),30720k(ubi),30720k(ubi1),51200k(overlay)"
+	fi
+}
+
 platform_do_upgrade() {
 	local board=$(board_name)
 
@@ -20,6 +68,11 @@ platform_do_upgrade() {
 			nand_do_upgrade "$1"
 			;;
 		esac
+		;;
+	xiaomi,redmi-router-ax6000-stock)
+		CI_KERN_UBIPART=ubi_kernel
+		CI_ROOT_UBIPART=ubi
+		nand_do_upgrade "$1"
 		;;
 	*)
 		nand_do_upgrade "$1"
@@ -60,6 +113,16 @@ platform_copy_config() {
 			emmc_copy_config
 			;;
 		esac
+		;;
+	esac
+}
+
+platform_pre_upgrade() {
+	local board=$(board_name)
+
+	case "$board" in
+	xiaomi,redmi-router-ax6000-stock)
+		redmi_ax6000_nand_upgrade_cleanup
 		;;
 	esac
 }
