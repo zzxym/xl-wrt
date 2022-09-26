04731443b2a1f86d5380d4d81a187ceee99c739f
Chen Minqiang <ptpt52@gmail.com>
2022-09-27 07:24:17 +0800

    kernel: fs-ntfs3: add depends

--- package/kernel/linux/modules/fs.mk
+++ package/kernel/linux/modules/fs.mk
@@ -662,6 +662,7 @@ $(eval $(call KernelPackage,fuse))
 define KernelPackage/fs-ntfs3
   SUBMENU:=$(FS_MENU)
   TITLE:=Ntfs3 support
+  DEPENDS:=@(LINUX_5_15||LINUX_5_19) +kmod-nls-base
   KCONFIG:= CONFIG_NTFS3_FS CONFIG_NTFS3_FS_POSIX_ACL=y
   FILES:=$(LINUX_DIR)/fs/ntfs3/ntfs3.ko
   $(call AddDepends/nls)
