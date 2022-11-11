jacelift/x-wrt_jacelift

ath79: init support for xwrt_mk-v0201

target/linux/ath79/image/generic.mk
@@ -703,6 +703,14 @@ define Device/xiaomi_mi-router-4q
 endef
 TARGET_DEVICES += xiaomi_mi-router-4q
 
+define Device/xwrt_mk-v0201
+  ATH_SOC := ar9344
+  DEVICE_TITLE := XWRT MK-V0201
+  DEVICE_PACKAGES := kmod-usb-core kmod-usb2 kmod-usb-ledtrig-usbport kmod-leds-gpio rssileds
+  IMAGE_SIZE := 16000k
+endef
+TARGET_DEVICES += xwrt_mk-v0201
+
 define Device/yuncore_a770
   ATH_SOC := qca9531
   DEVICE_TITLE := YunCore A770

zzxym/xl-wrt

target/linux/ath79/image/generic.mk
@@ -2752,6 +2752,15 @@ define Device/xiaomi_mi-router-4q
 endef
 TARGET_DEVICES += xiaomi_mi-router-4q
 
+define Device/xwrt_mk-v0201
+  SOC := ar9344
+  DEVICE_VENDOR := XWRT
+  DEVICE_MODEL := MK-V0201
+  DEVICE_PACKAGES := kmod-usb-core kmod-usb2 kmod-usb-ledtrig-usbport kmod-leds-gpio rssileds
+  IMAGE_SIZE := 16000k
+endef
+TARGET_DEVICES += xwrt_mk-v0201
+
 define Device/yuncore_a770
   SOC := qca9531
   DEVICE_VENDOR := YunCore