1f6766732a518da091fe6bb43898ae63751ac6aa
Chen Minqiang <ptpt52@gmail.com>
2023-03-19 10:17:08 +0800

    mediatek: init add support for tenbay_ac-2210e

--- target/linux/mediatek/image/filogic.mk
+++ target/linux/mediatek/image/filogic.mk
@@ -368,3 +368,25 @@ define Device/zyxel_ex5601-t0-stock
   DTC_FLAGS += -@ --space 32768
 endef
 TARGET_DEVICES += zyxel_ex5601-t0-stock
+
+define Device/tenbay_ac-2210e
+  DEVICE_VENDOR := Tenbay
+  DEVICE_MODEL := AC-2210E
+  DEVICE_DTS := mt7981-tenbay_ac-2210e
+  DEVICE_DTS_DIR := ../dts
+  DEVICE_PACKAGES := kmod-mdio-gpio kmod-i2c-gpio i2c-tools
+  SUPPORTED_DEVICES := tenbay,ac-2210e
+  UBINIZE_OPTS := -E 5
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  IMAGE_SIZE := 65536k
+  KERNEL_IN_UBI := 1
+  IMAGES += factory.bin
+  IMAGE/factory.bin := append-ubi | check-size $$$$(IMAGE_SIZE)
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+  KERNEL = kernel-bin | lzma | \
+	fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb
+  KERNEL_INITRAMFS = kernel-bin | lzma | \
+	fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb with-initrd
+endef
+TARGET_DEVICES += tenbay_ac-2210e
