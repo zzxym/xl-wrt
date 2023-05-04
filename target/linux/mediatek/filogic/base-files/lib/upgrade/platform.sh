bb316b0358e4b79813a16c80fb308f38d655d3a5
Chen Minqiang <ptpt52@gmail.com>
2023-05-04 16:38:44 +0800

    mediatek: init add tenbay_ms3000k

--- target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
+++ target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
@@ -149,6 +149,9 @@ platform_do_upgrade() {
 	tenbay,wr3000k-gsw-emmc-nor)
 		tenbay_mmc_do_upgrade_dual_boot "$1"
 		;;
+	tenbay,ms3000k)
+		default_do_upgrade "$1"
+		;;
 	*)
 		nand_do_upgrade "$1"
 		;;
@@ -171,6 +174,7 @@ platform_check_image() {
 		}
 		return 0
 		;;
+	tenbay,ms3000k|\
 	tenbay,wr3000k-gsw-emmc-nor)
 		return 0
 		;;
