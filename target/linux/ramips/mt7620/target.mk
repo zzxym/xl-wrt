b79bfd23209f67d0a79f7390f844fa6512900148
Chen Minqiang <ptpt52@gmail.com>
2023-08-14 21:03:15 +0800

    ramips: mt7620: switch to kernel 5.10

--- target/linux/ramips/mt7620/target.mk
+++ target/linux/ramips/mt7620/target.mk
@@ -7,6 +7,8 @@ BOARDNAME:=MT7620 based boards
 FEATURES+=usb nand ramdisk
 CPU_TYPE:=24kc
 
+KERNEL_PATCHVER:=5.10
+
 DEFAULT_PACKAGES += kmod-rt2800-soc wpad-basic-mbedtls swconfig
 
 define Target/Description
