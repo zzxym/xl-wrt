f02c9c22153a72d298e6951bfc1a136e1f08d0c7
Chen Minqiang <ptpt52@gmail.com>
2023-09-14 15:00:21 +0800

    comgt: retry if modem/device not ready

--- package/network/utils/comgt/files/ncm.sh
+++ package/network/utils/comgt/files/ncm.sh
@@ -57,8 +57,8 @@ proto_ncm_setup() {
 
 	device="$(readlink -f $device)"
 	[ -e "$device" ] || {
-		echo "Control device not valid"
-		proto_set_available "$interface" 0
+		echo "Control device not valid, wait.."
+		sleep 1
 		return 1
 	}
 
@@ -117,9 +117,9 @@ proto_ncm_setup() {
 	json_load "$(cat /etc/gcom/ncm.json)"
 	json_select "$manufacturer"
 	[ $? -ne 0 ] && {
-		echo "Unsupported modem"
+		echo "Unsupported modem=$manufacturer"
 		proto_notify_error "$interface" UNSUPPORTED_MODEM
-		proto_set_available "$interface" 0
+		sleep 1
 		return 1
 	}
 
@@ -244,8 +244,8 @@ proto_ncm_teardown() {
 
 	device="$(readlink -f $device)"
 	[ -e "$device" ] || {
-		echo "Control device not valid"
-		proto_set_available "$interface" 0
+		echo "Control device not valid, wait .."
+		sleep 1
 		return 1
 	}
 
@@ -269,7 +269,7 @@ proto_ncm_teardown() {
 
 	json_load "$(cat /etc/gcom/ncm.json)"
 	json_select "$manufacturer" || {
-		echo "Unsupported modem"
+		echo "Unsupported modem=$manufacturer"
 		proto_notify_error "$interface" UNSUPPORTED_MODEM
 		return 1
 	}
